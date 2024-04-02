ea71bd44c893ffeb06b6fbddd7fe343625515a87
saurabh kumar <saurabhbabu287@gmail.com>
2024-04-02 18:32:25 +0530

    changed files

---
+++ client/src/lib/navbar.js
@@ -0,0 +1,35 @@
+
+const navbaritems = [
+    {
+        id: 1,
+        name: "Home",
+        url: "/"
+      },
+      {
+        id: 2,
+        name: "Events",
+        url: "/events"
+      },
+      {
+        id: 3,
+        name: "Projects",
+        url: "/projects"
+      },
+      {
+        id: 4,
+        name: "Blogs",
+        url: "/blogs"
+      },
+      {
+        id: 5,
+        name: "About Us",
+        url: "/about-us"
+      },
+      {
+        id: 6,
+        name: "Contact Us",
+        url: "/contact-us"
+      }
+]
+
+export default navbaritems;
